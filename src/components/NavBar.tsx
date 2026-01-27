ee2ea89a3453cced89ca5ea327b7436ccdbd224b
eric-laudrum <ericlaudrum@gmail.com>
2026-01-27 00:48:54 -0500

    Create nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+const NavBar = () => {
+    return(
+        <nav>
+            <div>
+                <Link href="/">Eric Laudrum</Link>
+            </div>
+            <div>
+                <Link href='/projects'>Projects</Link>
+                <Link href='/about'>About</Link>
+                <Link href='/contact'>Contact</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
