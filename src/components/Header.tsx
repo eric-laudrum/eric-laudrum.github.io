9c20b4589e7cfea5465eecafd95d1b3aae386556
eric-laudrum <ericlaudrum@gmail.com>
2026-01-28 00:32:37 -0500

    Update header and nav bar for cleaner layout and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header">
             <h1 className="banner-title"><Link href="/">Eric Laudrum</Link></h1>
         
-            
+            <NavBar />
         </div>
         </>
 
