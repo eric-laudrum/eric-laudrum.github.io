dfbb48bb20d16dfc59f542d032aa9db7650851c9
eric-laudrum <ericlaudrum@gmail.com>
2026-03-07 17:29:55 -0500

    config/set up to launch on git pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+    basePath: '/eric-laudrum-portfolio',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
