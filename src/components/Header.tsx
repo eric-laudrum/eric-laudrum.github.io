01b1fa8b67449fa2801ce30f7b3e38874fb8b89f
eric-laudrum <ericlaudrum@gmail.com>
2026-03-22 18:41:18 -0400

    add/ styled image to replace text header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,10 @@ const Header = () => {
         <>
         <div className="header">
             <h1 className="banner-title">
-                <Link className="title" href="/">Eric Laudrum</Link>
+                <Link className="title" href="/">
+                
+                    <img src="/assets/eric-nameplate.png" alt="Eric Laudrum" />
+                </Link>
             </h1>
         
             <NavBar />
